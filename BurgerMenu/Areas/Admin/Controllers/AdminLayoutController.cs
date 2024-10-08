6b994562d6b9170ded9a4677e66efa9ec0017240
Batuhan18 <54512066+Batuhan18@users.noreply.github.com>
2024-10-08 15:43:55 +0300

    ...

--- BurgerMenu/Areas/Admin/Controllers/AdminLayoutController.cs
+++ BurgerMenu/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -13,5 +13,20 @@ namespace BurgerMenu.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialSidebar()
+        {
+            return PartialView();
+        }
     }
 }
